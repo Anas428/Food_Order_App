ad3372133ad87cc036e34481b6f4557f8a221463
bunny <anasnadeem428@gmail.com>
2021-06-29 02:47:43 +0500

    Pages Are Done

--- app/src/main/java/com/android/food_hub/Activity/ShowDetailActivity.java
+++ app/src/main/java/com/android/food_hub/Activity/ShowDetailActivity.java
@@ -1,14 +1,16 @@
-package com.android.food_hub;
+package com.android.food_hub.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.food_hub.R;
+
+public class ShowDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_show_detail);
     }
 }
